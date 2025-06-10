0679ab96484a5f4984fcbb272c43c3d62ed979a0
Leonaro Rodrigues <lrodriguesdasilva@gmail.com>
2025-06-10 15:16:07 -0300

    Ajustes de properties

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+    build: {
+        outDir: 'dist'
+    },
+
+    server: {
+        proxy: {
+            '/api': 'https://seu-backend.railway.app'
+        }
+    },
+
+    plugins: [react()],
 });
